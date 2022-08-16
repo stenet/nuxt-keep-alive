33cdceffb1ab9becc4ce320fe9b409c2a438fe5d
Stefan Heim <stefan.heim@tip.co.at>
2022-08-16 15:08:58 +0200

    demo

---
+++ app.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div>
+    <nav>
+      <NuxtLink to="/">index</NuxtLink>
+      <NuxtLink to="/sub/123">sub 123</NuxtLink>
+    </nav>
+    
+    <NuxtPage></NuxtPage>
+<!--    <RouterView v-slot="{ Component, route }">
+      <KeepAlive :max="2">
+        <Component :is="Component" :key="route.path"/>
+      </KeepAlive>
+    </RouterView>-->
+  </div>
+</template>
+
+<style>
+nav {
+  display: flex;
+  gap: 1rem;
+}
+</style>
---
+++ pages/sub/[id].vue
@@ -0,0 +1,18 @@
+﻿<script lang="ts" setup>
+const visible = ref(false);
+
+setTimeout(() => {
+  visible.value = true;
+}, 200);
+</script>
+
+<template>
+  <div v-if="visible">
+    <h1>Level 01/Page 01</h1>
+    <input type="text">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
